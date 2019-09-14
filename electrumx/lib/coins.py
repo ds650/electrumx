ea02b9bd6cd86f30457914c28992ef4fc11377fb
ds650 <39422200+ds650@users.noreply.github.com>
2019-09-15 00:31:41 +0100

    Update coins.py

--- electrumx/lib/coins.py
+++ electrumx/lib/coins.py
@@ -2949,11 +2949,8 @@ class Cryptoescudo(Coin):
     RPC_PORT = 61142
     REORG_LIMIT = 1000
     PEERS = [
-        'electrum1.cryptoescudo.org t s',
-        'electrum2.cryptoescudo.org t s',
-        'electrum3.cryptoescudo.org t s',
-        'electrum4.cryptoescudo.pt t s',
-        'electrum5.cryptoescudo.pt t s',
+        'electrum1.cryptoescudo.org t',
+        'electrum2.cryptoescudo.org t',
         'electrumx1.cryptoescudo.eu t s'
         
     ]
